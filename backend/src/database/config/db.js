e30fd0eb7ee8726c25dee28edd51ae34b1f5ca7f
Медведев Сергей <c0nterdash@gmail.com>
2026-04-24 11:30:47 +0500

    release_preparing

--- backend/src/database/config/db.js
+++ backend/src/database/config/db.js
@@ -2,16 +2,12 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD || '',
-  port: process.env.DB_PORT,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
-console.log('Connecting to DB:', {
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
-});
+
+console.log('Connecting via DATABASE_URL');
+
 module.exports = pool;
